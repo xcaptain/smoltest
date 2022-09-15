c56e8942b2c69bd4b51ccdbeaf90e2022c04d6c7
Joey <joey.xie@deeper.network>
2022-09-15 15:16:25 +0800

    add doc and ws

--- test.js
+++ test.js
@@ -1,34 +1,46 @@
 import { start } from '@substrate/smoldot-light';
 import * as fs from 'fs';
+import { WebSocketServer } from 'ws';
 
 async function test10() {
   const chainSpec = fs.readFileSync('./dev.json', 'utf8');
 
   const client = start();
-  const chain = await client.addChain({
+
+  await client.addChain({
     chainSpec,
-    jsonRpcCallback: (jsonRpcResponse) => {
-      console.log('response:', jsonRpcResponse);
+    jsonRpcCallback: (resp) => {
+      connection.send(resp);
     }
   });
-
-  chain.sendJsonRpc('{"jsonrpc":"2.0","id":1,"method":"chain_getBlock","params":[]}');
-}
-
-async function test7() {
-  const chainSpec = fs.readFileSync('./deeper-chain-7.json', 'utf8');
-
-  const client = start();
-  const chain = await client.addChain({
-    chainSpec,
-    jsonRpcCallback: (jsonRpcResponse) => {
-      console.log('response:', jsonRpcResponse);
-    }
+  let wsServer = new WebSocketServer({
+    port: 9944
   });
 
-  chain.sendJsonRpc('{"jsonrpc":"2.0","id":1,"method":"chain_getBlock","params":[]}');
-  chain.sendJsonRpc('{"jsonrpc":"2.0","id":1,"method":"system_health","params":[]}');
-  chain.sendJsonRpc('{"jsonrpc":"2.0","id":1,"method":"chain_getBlock","params":[]}');
+  wsServer.on('connection', async function (connection, request) {
+    const chain = await client.addChain({
+      chainSpec,
+      jsonRpcCallback: (resp) => {
+        console.log('response:', resp);
+        connection.send(resp);
+      }
+    });
+
+    connection.on('message', function (data, isBinary) {
+      if (!isBinary) {
+        const message = data.toString('utf8');
+        chain.sendJsonRpc(message);
+      } else {
+        connection.close(1002); // Protocol error
+      }
+    });
+
+    // When the connection closes, remove the chains that have been added.
+    connection.on('close', function (reasonCode, description) {
+      console.log("(demo) JSON-RPC client " + request.socket.remoteAddress + ' disconnected.');
+      chain.remove();
+    });
+  });
 }
 
 test10();
